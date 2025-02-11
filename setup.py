67f69667a232f657374e1a97866f98dae8152230
Rizal A.S <70193645+rizaru-desu@users.noreply.github.com>
2025-02-11 22:37:55 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ setup(
         ]
     },
     data_files=[
-        ("/etc/zas", ["etc/network.scenario"]),
-        ("/etc", ["etc/zas_scenario.cfg"]),
-        ("/usr/share/zas_agent", ["doc/zas-agent-0.1.1.pdf"]),
-        ("/usr/share/man/man1", ["doc/zas_agent.py.1"])
+        ('~/.zas', ['etc/network.scenario'])
+        ("~/etc", ["etc/zas_scenario.cfg"]),
+        ("~/usr/share/zas_agent", ["doc/zas-agent-0.1.1.pdf"]),
+        ("~/usr/share/man/man1", ["doc/zas_agent.py.1"])
     ],
     install_requires=[
         "daemonize==2.4.2",
